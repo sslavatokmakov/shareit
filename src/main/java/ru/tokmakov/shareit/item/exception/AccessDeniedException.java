71c4a747cb4f79b4c9b5b104825146d45f8c9d21
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-09-28 12:43:50 +0300

    style: formatted

--- src/main/java/ru/tokmakov/shareit/item/exception/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package ru.tokmakov.shareit.item.exception;
-
-public class AccessDeniedException extends RuntimeException {
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
